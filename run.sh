26c6ec5c6508e467caec2470de973fcd56e45c36
EXT Sara Gilbert <sara.gilbert@qt.io>
2023-07-13 06:48:32 +0300

    only run exghibition_court for ec_test

--- run.sh
+++
@@ -1,4 +0,0 @@
-pabot --testlevelsplit suites --variable HEADLESS:True --variable URL:https://test4.omenahotels.com/ --argumentfile
-pabot --testlevelsplit suites --variable HEADLESS:True --variable URL:https://test3.omenahotels.com/
-
-#robot suites
