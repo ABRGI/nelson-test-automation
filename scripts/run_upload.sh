846098483e5c545dfe82e6ad78edd7d3f7a07ad7
Sara Gilbert <sara.gilbert@softico.fi>
2023-07-18 10:01:01 +0300

    debug#

--- scripts/run_upload.sh
+++ scripts/run_upload.sh
@@ -1,8 +1,7 @@
 
-robot -A environments/test4.txt \
---include pretest \
---outputdir reports/test4 \
-testsuites/
+#robot -A environments/test4.txt \
+#--outputdir reports/test4 \
+#testsuites/
 
 
 testmo automation:run:submit --instance https://softico.testmo.net --project-id 1 --name "Robot test run" --source "CI" --results reports/test4/*.xml
